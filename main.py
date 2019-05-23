5c21a9b086734f0b31e8b098f8ceb1b6e94ddce3
ACab <andr3a.cbb@gmail.com>
2019-05-23 17:52:28 +0200

    improvements in results plots

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ Created on Tue May  7 14:55:17 2019
 
 @author: acabbia
 """
-from datetime import datetime
 import os
 import cobra
 import pandas as pd
@@ -24,15 +23,16 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.decomposition import KernelPCA
 
 from skbio import DistanceMatrix
+from skbio.stats.distance import mantel
 from skbio.tree import nj
 from ete3 import Tree, TreeStyle
 from ete3 import NCBITaxa
 
 #%%
 # loading models and data structures functions
-
 
 def binary(model, ref_model):
 
@@ -61,8 +61,11 @@ def binary(model, ref_model):
 
     return rxns, mets, genes
 
-
-def modelNet(model):
+def modelNet(model, remove_hub_metabolites = False):
+    
+    if remove_hub_metabolites:
+        model = remove_currency_metabolites(model)
+    
     #Returns a grakel.Graph object from a cobra.model object
 
     edges_in = []
@@ -92,7 +95,6 @@ def modelNet(model):
 
     return g
 
-
 def FBA(model, ref_model):
 
     ###### set obj and (minimal) bounds
@@ -107,7 +109,6 @@ def FBA(model, ref_model):
     sol = model.optimize()
     # flux distributions are appended following the index
     return sol.fluxes
-
 
 def load_library(path, ref_model_path):
     '''
@@ -143,7 +144,7 @@ def load_library(path, ref_model_path):
         gene_matrix[label] = genes
 
         # 2: make graphlist
-        graphlist.append(modelNet(model))
+        graphlist.append(modelNet(model, remove_hub_metabolites=True))
 
         # 3: make flux matrix
         fluxes = FBA(model, ref_model)
@@ -151,25 +152,84 @@ def load_library(path, ref_model_path):
 
     return reactions_matrix, metabolite_matrix, gene_matrix, graphlist, sol_df
 
+
+def remove_currency_metabolites(model):
+    # removes the 10 more connected metabolites
+    # before the topology analysis (GK)
+    
+    ids = []
+    num = []
+
+    for met in model.metabolites:
+        ids.append(met.id)
+        num.append(len(met.reactions))
+    
+    currency_met_df = pd.Series(data=num, index = ids)
+    currency_met_df.sort_values(ascending = False, inplace=True)
+    currency_met_list = list(currency_met_df.head(10).index)
+    
+    for m in currency_met_list:
+        model.metabolites.get_by_id(m).remove_from_model()
+    
+    return model
+
 # Exploratory analysis functions
 
-
-def boxplots(df, label):
+def boxplots(df, label, outfolder, filename):
     # Reactions/metabolites/genes content of the models, grouped by label
-
+    
     groups = df.T.sum(axis=1).groupby(label)
 
     names = []
     data = []
 
     for g in groups:
-        names.append(g[0])
+        names.append(g[0]+'\n n='+str(g[1].count()))
         data.append(g[1].values)
-
+       
     ax = sns.boxplot(data=data)
+    
     ax.set_xticklabels(labels=names, rotation=90)
+    ax.tick_params(axis='x', labelsize=15)
+    ax.tick_params(axis='y', labelsize=13)
+    
+    ax.get_figure().savefig(outfolder+'boxplots/'+filename+'.png', dpi=300, bbox_inches='tight')
+    
+    plt.show()
 
-    #ax.get_figure().savefig(outfolder+'boxplots/'+c+'.png', dpi=1200, bbox_inches='tight')
+def KPCA(DM, label, outfolder, filename):
+    # Kernel-PCA 2-D scatterplot
+    kpca = KernelPCA(kernel="precomputed", n_components=2 , n_jobs=-1)
+    X_kpca = kpca.fit_transform(1-DM)
+    
+    
+    g = sns.scatterplot(x = X_kpca[:,0] , y = X_kpca[:,1], hue = label, legend = 'brief')
+    g.tick_params(axis='x', labelsize=14)
+    g.tick_params(axis='y', labelsize=14)
+    
+    box = g.get_position() # get position of figure
+    g.set_position([box.x0, box.y0, box.width, box.height]) # resize position
+    # Put a legend to the right side
+    plt.legend(loc='center right', bbox_to_anchor=(1.5, 0.5), ncol=1)
+    g.get_figure().savefig(outfolder+'K_PCA/'+filename+'.png', dpi=300, bbox_inches='tight')
+
+    plt.show(g)
+
+def plot_C2_results(df, outfolder, filename):
+    ### plots and saves Clustering and Classification results (hi rez)
+    ax = df.plot(kind = 'bar', fontsize = 14).legend(bbox_to_anchor=(1, 0.5))  
+    ax.get_figure().savefig(outfolder+'class_clust_results/'+filename+'.png', dpi=300, bbox_inches='tight')
+    
+    plt.show()
+
+def plot_tree_comparisons(df,outfolder, filename):
+    ### plots and saves Tree comparison results (hi rez)
+    ax = df.plot(kind = 'bar', fontsize = 14)
+    ax.legend(bbox_to_anchor=(1, 0.5))
+    ax.set_xticklabels(labels = list(df.index),rotation=0)
+
+    ax.get_figure().savefig(outfolder+'tree_comparisons/'+filename+'.png', dpi=300, bbox_inches='tight')
+    
     plt.show()
 
 # distance matrix functions
@@ -205,7 +265,6 @@ def FBA_cosine_DM(df):
 
 # Clustering functions
 
-
 def CalculateAccuracy(y, y_hat):
 
     accuracy = 0
@@ -235,7 +294,7 @@ def CalculateAccuracy(y, y_hat):
 
 
 def HCClust(DM, trueLabel):
-
+    
     HC = AgglomerativeClustering(n_clusters=len(
         pd.Series(trueLabel).unique()), affinity='precomputed', linkage='average').fit(DM)
     y_pred = HC.labels_
@@ -256,7 +315,6 @@ def SCClust(DM, trueLabel):
     return accSC, bestLabSC, cmSC
 
 # classification functions
-
 
 def classify(DM, truelabel):
     # performs (10-fold CV) classification (with SVM and KNN), prints accuracy of retrieval of original labels
@@ -283,7 +341,6 @@ def classify(DM, truelabel):
     return scores_K_NN, scores_K_SVM
 
 # PhyloTree functions
-
 
 def make_tree(DM):
 
@@ -318,8 +375,9 @@ path_AGORA = '/home/acabbia/Documents/Muscle_Model/models/AGORA_1.03/'
 path_ref_PDGSMM = '/home/acabbia/Documents/Muscle_Model/models/HMR2.xml'
 path_ref_AGORA = '/home/acabbia/Documents/Muscle_Model/models/AGORA_universe.xml'
 
+outfolder = '/home/acabbia/Dropbox/BMC_Distance2019/figures/'
 
-### Labels
+### LABELS
 label_PDGSM = [s.split('_')[0] for s in sorted(os.listdir(path_PDGSMM))]
 
 AGORA_taxonomy = pd.read_csv('/home/acabbia/Documents/Muscle_Model/GSMM-distance/agora_taxonomy.tsv',
@@ -331,16 +389,18 @@ AGORA_taxonomy.replace(np.nan, 'Other', inplace=True)
 # Replaces and aggregates classes with less than 10 samples into a new "Other" class
 # to reduce the number of classes and computational time of the clustering
 
-for c in ['phylum', 'oxygenstat', 'gram', 'mtype', 'metabolism']:
+for c in ['phylum', 'oxygenstat', 'gram', 'mtype']:
     for s in list(AGORA_taxonomy[c].value_counts()[AGORA_taxonomy[c].value_counts() < 10].index):
         AGORA_taxonomy[c].replace(s, 'Other', inplace=True)
 
-# Merge 'Nanaerobe' with "Microaerophile" in oxygenstat variable
-AGORA_taxonomy['oxygenstat'].replace(
-    'Nanaerobe', 'Microaerophile', inplace=True)
+# Merge more labels in oxy gram and type variables
+AGORA_taxonomy['oxygenstat'].replace('Nanaerobe', 'Microaerophile', inplace=True)
+AGORA_taxonomy['oxygenstat'].replace('Obligate anaerobe', 'Anaerobe', inplace=True)
+AGORA_taxonomy['oxygenstat'].replace('Obligate aerobe', 'Aerobe', inplace=True)
+AGORA_taxonomy['gram'].replace('Uncharacterized', 'Other', inplace=True)
+AGORA_taxonomy['mtype'].replace('Uncharacterized', 'Other', inplace=True)
 
-
-# Make labels
+# Make final labels
 label_AGORA_phylum = list(AGORA_taxonomy['phylum'].values)
 label_AGORA_oxy = list(AGORA_taxonomy['oxygenstat'].values)
 label_AGORA_gram = list(AGORA_taxonomy['gram'].values)
@@ -355,32 +415,6 @@ rxns_AGORA, met_AGORA, gene_AGORA, graphlist_AGORA, flux_AGORA = load_library(
     path_AGORA, path_ref_AGORA)
 
 #%%
-# Explorative Data Analysis (boxplots)
-# Reactions/metabolites/(genes) content of the models, grouped by label
-
-boxplots(rxns_AGORA, label_AGORA_gram)
-boxplots(rxns_AGORA, label_AGORA_oxy)
-boxplots(rxns_AGORA, label_AGORA_phylum)
-boxplots(rxns_AGORA, label_AGORA_type)
-boxplots(rxns_PDGSM, label_PDGSM)
-
-boxplots(met_AGORA, label_AGORA_gram)
-boxplots(met_AGORA, label_AGORA_oxy)
-boxplots(met_AGORA, label_AGORA_phylum)
-boxplots(met_AGORA, label_AGORA_type)
-boxplots(met_PDGSM, label_PDGSM)
-
-'''
-### Gene annotation inexistent(PDGSM) or problematic (AGORA) 
-
-boxplots(gene_AGORA, label_AGORA_gram)
-boxplots(gene_AGORA, label_AGORA_oxy)
-boxplots(gene_AGORA, label_AGORA_phylum)
-boxplots(gene_AGORA, label_AGORA_type)
-boxplots(gene_PDGSM, label_PDGSM)
-'''
-#%%
-
 # make jaccard distance matrices
 JD_PDGSM = jaccard_DM(rxns_PDGSM)
 JD_AGORA = jaccard_DM(rxns_AGORA)
@@ -392,6 +426,44 @@ GK_AGORA = gKernel_DM(graphlist_AGORA)
 # make FBA cosine distance matrices
 COS_PDGSM = FBA_cosine_DM(flux_PDGSM)
 COS_AGORA = FBA_cosine_DM(flux_AGORA)
+
+#%%
+# Explorative Data Analysis (boxplots)
+# Reactions/metabolites/(genes) content of the models, grouped by label
+boxplots(rxns_AGORA, label_AGORA_gram, outfolder, 'rxns_AGORA_gram')
+boxplots(rxns_AGORA, label_AGORA_oxy, outfolder, 'rxns_AGORA_oxy')
+boxplots(rxns_AGORA, label_AGORA_phylum, outfolder,  'rxns_AGORA_phylum')
+boxplots(rxns_AGORA, label_AGORA_type, outfolder,  'rxns_AGORA_type')
+boxplots(rxns_PDGSM, label_PDGSM, outfolder, 'rxns_PDGSM')
+
+boxplots(met_AGORA, label_AGORA_gram, outfolder,  'mets_AGORA_gram')
+boxplots(met_AGORA, label_AGORA_oxy, outfolder,  'mets_AGORA_oxy')
+boxplots(met_AGORA, label_AGORA_phylum, outfolder,  'mets_AGORA_phylum')
+boxplots(met_AGORA, label_AGORA_type, outfolder, 'mets_AGORA_type')
+boxplots(met_PDGSM, label_PDGSM, outfolder, 'mets_PDGSM')
+
+#%%  
+# Explorative Data Analysis (Kernel-PCA)
+
+KPCA(JD_AGORA, label_AGORA_type, outfolder , 'JD_AGORA_type')
+KPCA(GK_AGORA, label_AGORA_type, outfolder , 'GK_AGORA_type')
+KPCA(COS_AGORA, label_AGORA_type, outfolder , 'COS_AGORA_type')
+
+KPCA(JD_AGORA, label_AGORA_phylum, outfolder , 'JD_AGORA_phy')
+KPCA(GK_AGORA, label_AGORA_phylum, outfolder , 'GK_AGORA_phy')
+KPCA(COS_AGORA, label_AGORA_phylum, outfolder , 'COS_AGORA_phy')
+
+KPCA(JD_AGORA, label_AGORA_oxy, outfolder , 'JD_AGORA_oxy')
+KPCA(GK_AGORA, label_AGORA_oxy, outfolder , 'GK_AGORA_oxy')
+KPCA(COS_AGORA, label_AGORA_oxy, outfolder , 'COS_AGORA_oxy')
+
+KPCA(JD_AGORA, label_AGORA_gram, outfolder , 'JD_AGORA_gram')
+KPCA(GK_AGORA, label_AGORA_gram, outfolder , 'GK_AGORA_gram')
+KPCA(COS_AGORA, label_AGORA_gram, outfolder , 'COS_AGORA_gram')
+
+KPCA(JD_PDGSM, label_PDGSM, outfolder , 'JD_PDGSM')
+KPCA(GK_PDGSM, label_PDGSM, outfolder , 'GK_PDGSM')
+KPCA(COS_PDGSM, label_PDGSM, outfolder , 'COS_PDGSM')
 
 #%%
 ##### Clustering
@@ -498,20 +570,21 @@ GK_SC_ACC_list = [GK_SC_PDGSM_acc, GK_SC_AGORA_acc_gram, GK_SC_AGORA_acc_oxy,
 COS_SC_ACC_list = [COS_SC_PDGSM_acc, COS_SC_AGORA_acc_gram, COS_SC_AGORA_acc_oxy,
                    COS_SC_AGORA_acc_phylum, COS_SC_AGORA_acc_type]
 
-#### PLOT CLUSTERING RESULTS
+# MAKE DF
 HC_Clust_results_df = pd.DataFrame(index=['PDGSM', 'AGORA-Gram', 'AGORA-Oxygen', 'AGORA-Phylum', 'AGORA-Type'])
 HC_Clust_results_df['Reaction Similarity (Jaccard)'] = JD_HC_ACC_list
 HC_Clust_results_df['Network Similarity (Graph Kernel)'] = GK_HC_ACC_list
 HC_Clust_results_df['Flux vector similarity (Cosine)'] = COS_HC_ACC_list
-HC_Clust_results_df.plot.bar(title='Hierarchical clustering: accuracy')
 
 
 SC_Clust_results_df = pd.DataFrame(index=['PDGSM', 'AGORA-Gram', 'AGORA-Oxygen', 'AGORA-Phylum', 'AGORA-Type'])
 SC_Clust_results_df['Reaction Similarity (Jaccard)'] = JD_SC_ACC_list
 SC_Clust_results_df['Network Similarity (Graph Kernel)'] = GK_SC_ACC_list
 SC_Clust_results_df['Flux vector similarity (Cosine)'] = COS_SC_ACC_list
-SC_Clust_results_df.plot.bar(title='Spectral clustering: accuracy')
 
+#### PLOT CLUSTERING RESULTS
+plot_C2_results(HC_Clust_results_df, outfolder,'HC_results')
+plot_C2_results(SC_Clust_results_df, outfolder,'SC_results')
 
 #%%
 #### classification
@@ -537,35 +610,33 @@ COS_AGORA_knn_phylum, COS_AGORA_svm_phylum = classify(
     COS_AGORA, label_AGORA_phylum)
 COS_AGORA_knn_type, COS_AGORA_svm_type = classify(COS_AGORA, label_AGORA_type)
 
-
-# plot KNN results
+# Collect results
 JD_KNN_ACC_list = [JD_PDGSM_knn.mean(), JD_AGORA_knn_gram.mean(),JD_AGORA_knn_oxy.mean(),JD_AGORA_knn_phylum.mean(),JD_AGORA_knn_type.mean()]
 GK_KNN_ACC_list = [GK_PDGSM_knn.mean(), GK_AGORA_knn_gram.mean(),GK_AGORA_knn_oxy.mean(),GK_AGORA_knn_phylum.mean(),GK_AGORA_knn_type.mean()]
 COS_KNN_ACC_list = [COS_PDGSM_knn.mean(), COS_AGORA_knn_gram.mean(),COS_AGORA_knn_oxy.mean(),COS_AGORA_knn_phylum.mean(),COS_AGORA_knn_type.mean()]
 
+JD_SVM_ACC_list = [JD_PDGSM_svm.mean(), JD_AGORA_svm_gram.mean(),JD_AGORA_svm_oxy.mean(),JD_AGORA_svm_phylum.mean(),JD_AGORA_svm_type.mean()]
+GK_SVM_ACC_list = [GK_PDGSM_svm.mean(), GK_AGORA_svm_gram.mean(),GK_AGORA_svm_oxy.mean(),GK_AGORA_svm_phylum.mean(),GK_AGORA_svm_type.mean()]
+COS_SVM_ACC_list = [COS_PDGSM_svm.mean(), COS_AGORA_svm_gram.mean(),COS_AGORA_svm_oxy.mean(),COS_AGORA_svm_phylum.mean(),COS_AGORA_svm_type.mean()]
+
+# MAKE DF
 KNN_results_df = pd.DataFrame(index = ['PDGSM', 'AGORA-Gram', 'AGORA-Oxygen', 'AGORA-Phylum', 'AGORA-Type']) 
 KNN_results_df['Reaction Similarity (Jaccard)'] = JD_KNN_ACC_list
 KNN_results_df['Network Similarity (Graph Kernel)'] = GK_KNN_ACC_list
 KNN_results_df['Flux vector similarity (Cosine)'] = COS_KNN_ACC_list
-KNN_results_df.plot.bar(title='KNN Classification: accuracy')
-
-# plot SVM results
-JD_SVM_ACC_list = [JD_PDGSM_svm.mean(), JD_AGORA_svm_gram.mean(),JD_AGORA_svm_oxy.mean(),JD_AGORA_svm_phylum.mean(),JD_AGORA_svm_type.mean()]
-GK_SVM_ACC_list = [GK_PDGSM_svm.mean(), GK_AGORA_svm_gram.mean(),GK_AGORA_svm_oxy.mean(),GK_AGORA_svm_phylum.mean(),GK_AGORA_svm_type.mean()]
-COS_SVM_ACC_list = [COS_PDGSM_svm.mean(), COS_AGORA_svm_gram.mean(),COS_AGORA_svm_oxy.mean(),COS_AGORA_svm_phylum.mean(),COS_AGORA_svm_type.mean()]
 
 SVM_results_df = pd.DataFrame(index = ['PDGSM', 'AGORA-Gram', 'AGORA-Oxygen', 'AGORA-Phylum', 'AGORA-Type']) 
 SVM_results_df['Reaction Similarity (Jaccard)'] = JD_SVM_ACC_list
 SVM_results_df['Network Similarity (Graph Kernel)'] = GK_SVM_ACC_list
 SVM_results_df['Flux vector similarity (Cosine)'] = COS_SVM_ACC_list
-SVM_results_df.plot.bar(title='SVM Classification: accuracy')
 
+#### PLOT CLASSIFICATION RESULTS
+plot_C2_results(KNN_results_df, outfolder, 'KNN_results')
+plot_C2_results(SVM_results_df, outfolder, 'SVM_results')
 
 #%%
-
 ##### Trees comparison
 # make ref tree (NCBI)
-
 ncbi = NCBITaxa()
 ncbi.update_taxonomy_database()
 
@@ -573,17 +644,14 @@ NCBI_ID = list(AGORA_taxonomy['ncbiid'].dropna().values)
 NCBI_tree = ncbi.get_topology(NCBI_ID)
 
 # Ugly way to convert "phyloTree" obj into "Tree" obj for comparison with other trees
-NCBI_tree.write(
-    format=1, outfile="/home/acabbia/Documents/Muscle_Model/GSMM-distance/trees/NCBI_tree.nw")
-NCBI_tree = Tree(
-    "/home/acabbia/Documents/Muscle_Model/GSMM-distance/trees/NCBI_tree.nw", format=1)
+NCBI_tree.write(format=1, outfile="/home/acabbia/Documents/Muscle_Model/GSMM-distance/trees/NCBI_tree.nw")
+NCBI_tree = Tree("/home/acabbia/Documents/Muscle_Model/GSMM-distance/trees/NCBI_tree.nw", format=1)
 
 # fix non zero diagonal values in FBA DM
 COS_AGORA[COS_AGORA < 10e-10] = 0
 COS_PDGSM[COS_PDGSM < 10e-10] = 0
 
 # Convert Distance matrices into skbio.Distance_matrix objects
-
 JD_AGORA_DM = DistanceMatrix(JD_AGORA)
 GK_AGORA_DM = DistanceMatrix(GK_AGORA)
 COS_AGORA_DM = DistanceMatrix(COS_AGORA)
@@ -601,12 +669,18 @@ TREE_JD_PDGSM = make_tree(JD_PDGSM_DM)
 TREE_GK_PDGSM = make_tree(GK_PDGSM_DM)
 TREE_COS_PDGSM = make_tree(COS_PDGSM_DM)
 
-#### What to do with PDGSM Tree?
+#%% Mantel test of correlation between distance matrices
 
+mantel(JD_AGORA_DM, GK_AGORA_DM, 'pearson')
+mantel(JD_AGORA_DM, COS_AGORA_DM, 'pearson')
+mantel(GK_AGORA_DM, COS_AGORA_DM, 'pearson')
+
+mantel(JD_PDGSM_DM, GK_PDGSM_DM, 'pearson')
+mantel(JD_PDGSM_DM, COS_PDGSM_DM, 'pearson')
+mantel(GK_PDGSM_DM, COS_PDGSM_DM, 'pearson')
 
 #%%
 # dictionary to translate between model_taxonomy.index (GK and JD trees) and NCBI_id (NCBI tree)
-
 idx_str = [str(i) for i in list(AGORA_taxonomy.index)]
 NCBI_str = [str(i) for i in NCBI_ID]
 
@@ -627,14 +701,17 @@ JD_GK_AGORA = TREE_JD_AGORA.compare(TREE_GK_AGORA, unrooted=True)
 JD_COS_AGORA = TREE_JD_AGORA.compare(TREE_COS_AGORA, unrooted=True)
 GK_COS_AGORA = TREE_GK_AGORA.compare(TREE_COS_AGORA, unrooted=True)
 
-COMPARISON_REF_DF = pd.DataFrame(index= ['Normalized RF distance'])
-COMPARISON_REF_DF['Reaction Similarity (Jaccard)'] = REF_JD_AGORA['norm_rf']
-COMPARISON_REF_DF['Network Similarity (Graph Kernel)'] = REF_GK_AGORA['norm_rf']
-COMPARISON_REF_DF['Flux vector similarity (Cosine)'] = REF_COS_AGORA['norm_rf']
-COMPARISON_REF_DF.plot.bar(title='Normalized Robinson-Fould distance from reference taxonomy (NCBI)')
+# collect data 
+COMPARISON_REF_DF = pd.DataFrame(index= ['normalized RF distance'])
+COMPARISON_REF_DF['Reaction Similarity \n (Jaccard)'] = REF_JD_AGORA['norm_rf']
+COMPARISON_REF_DF['Network Similarity \n (Graph Kernel)'] = REF_GK_AGORA['norm_rf']
+COMPARISON_REF_DF['Flux vector similarity \n (Cosine)'] = REF_COS_AGORA['norm_rf']
 
-COMPARISON_DF = pd.DataFrame(index= ['Normalized RF distance'])
+COMPARISON_DF = pd.DataFrame(index= ['normalized RF distance'])
 COMPARISON_DF['Jaccard-GraphKernel'] = JD_GK_AGORA['norm_rf']
 COMPARISON_DF['Jaccard-Cosine'] = JD_COS_AGORA['norm_rf']
 COMPARISON_DF['GraphKernel-Cosine'] = GK_COS_AGORA['norm_rf']
-COMPARISON_DF.plot.bar(title='Normalized Robinson-Fould distance between metrics-based trees')
+
+#plot results
+plot_tree_comparisons(COMPARISON_DF, outfolder, 'distance_fromMetric')
+plot_tree_comparisons(COMPARISON_REF_DF, outfolder, 'distance_fromReference')
